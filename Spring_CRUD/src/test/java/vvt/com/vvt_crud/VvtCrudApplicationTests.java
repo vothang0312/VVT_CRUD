9dc6bb84777bcab0286787d90f79122c8570fc48
vothang0312 <vanthang26d@gmail.com>
2021-04-13 09:06:16 +0700

    Beo tao 2 project spring boot and angular

---
+++ Spring_CRUD/src/test/java/vvt/com/vvt_crud/VvtCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package vvt.com.vvt_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvtCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
